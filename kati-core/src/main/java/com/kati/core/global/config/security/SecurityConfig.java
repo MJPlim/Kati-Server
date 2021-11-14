4e985ca890e96e7e1de5aeaf776b52459efb36c1
Chanho Jung <cksgh3422@gmail.com>
2021-11-14 22:32:58 +0900

    :sparkles: Add cors config

--- kati-core/src/main/java/com/kati/core/global/config/security/SecurityConfig.java
+++ kati-core/src/main/java/com/kati/core/global/config/security/SecurityConfig.java
@@ -40,6 +40,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .addFilterBefore(corsFilter, SecurityContextPersistenceFilter.class);
+
+
         http
                 .csrf().disable()
                 .formLogin().disable()
