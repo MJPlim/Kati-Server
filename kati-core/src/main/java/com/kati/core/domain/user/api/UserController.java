ea7345c1463cc383b63c455795b52a952ea1f312
Chanho Jung <cksgh3422@gmail.com>
2021-11-21 20:08:25 +0900

    :fire: Modify Oauth2Login @Crossorigin

--- kati-core/src/main/java/com/kati/core/domain/user/api/UserController.java
+++ kati-core/src/main/java/com/kati/core/domain/user/api/UserController.java
@@ -125,7 +125,6 @@ public class UserController {
         return userService.userSummary(principal);
     }
 
-    @CrossOrigin(origins = {"http://3.38.97.234:8000/core-service/", "http://localhost:3000/"})
     @PostMapping(value = "/oauth2-login")
     public ResponseEntity<Oauth2LoginResponse> oauth2Login(@RequestBody Oauth2LoginRequest oauth2) throws ServletException, IOException {
 
