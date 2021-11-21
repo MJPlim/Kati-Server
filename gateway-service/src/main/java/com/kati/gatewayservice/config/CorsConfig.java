e6f54fae1db8a0fdea737902b674b39897908b9d
Harry-Bro <likeharrybro@gmail.com>
2021-11-21 16:06:02 +0900

    :wrench: Update CORS config

--- gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
+++ gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
@@ -9,6 +9,8 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import java.util.Collections;
+
 @Configuration
 public class CorsConfig implements WebFluxConfigurer {
 
@@ -28,7 +30,7 @@ public class CorsConfig implements WebFluxConfigurer {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
         corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
         UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
         corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
