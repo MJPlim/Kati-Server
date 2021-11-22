a32ddfd79fe405ba3c4f1e50d693cee61eacf916
Harry-Bro <likeharrybro@gmail.com>
2021-11-22 12:39:21 +0900

    :ambulance: Fix cors

--- gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
+++ gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
@@ -2,10 +2,16 @@ package com.kati.gatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class CorsConfig {
@@ -41,46 +47,45 @@ public class CorsConfig {
 //    private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
 //
 
-//    @Bean
-//    public WebFilter corsFilter() {
-//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-//            ServerHttpRequest request = ctx.getRequest();
-//            if (CorsUtils.isCorsRequest(request)) {
-//                ServerHttpResponse response = ctx.getResponse();
-//                HttpHeaders headers = response.getHeaders();
-////                headers.add("Access-Control-Allow-Origin", "*");
-//                headers.setAccessControlAllowOrigin("*");
-//                headers.setAccessControlExposeHeaders(ImmutableList.of("*"));
-//                headers.add("Access-Control-Allow-Methods", "*");
-////                headers.add("Access-Control-Allow-Headers", "*");
-//                headers.setAccessControlAllowCredentials(true);
-//                if (request.getMethod() == HttpMethod.OPTIONS) {
-//                    response.setStatusCode(HttpStatus.OK);
-//                    return Mono.empty();
-//                }
-//            }
-//            return chain.filter(ctx);
-//        };
-//    }
-
     @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedOrigin("http://localhost:3000");
-        corsConfiguration.setMaxAge(7200L);
-        corsConfiguration.addAllowedMethod(HttpMethod.GET);
-        corsConfiguration.addAllowedMethod(HttpMethod.POST);
-        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
-        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-        corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
-        corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addExposedHeader("*");
-        corsConfiguration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+//                headers.setAccessControlAllowCredentials(true);
+                headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
+                headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+                headers.add("Access-Control-Max-Age", "7200");
+                headers.add("Access-Control-Allow-Headers", "*");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
     }
+
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+////        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+//        corsConfiguration.setMaxAge(7200L);
+//        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+//        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+//        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+//        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+//        corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+//        corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
+//        corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addExposedHeader("*");
+//        corsConfiguration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+//        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+//    }
 
 }
