0badc5aafac55cc13a9d58fd6dbfc864dab4a97c
Harry-Bro <likeharrybro@gmail.com>
2021-11-22 11:07:45 +0900

    :ambulance: Fix cors

--- gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
+++ gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
@@ -3,9 +3,12 @@ package com.kati.gatewayservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Objects;
 
 @Configuration
 public class CorsConfig {
@@ -77,7 +80,11 @@ public class CorsConfig {
         corsConfiguration.addExposedHeader("*");
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource, (corsConfiguration1, serverWebExchange) -> {
+            if (Objects.equals(serverWebExchange.getRequest().getMethod(), HttpMethod.OPTIONS))
+                serverWebExchange.getResponse().setStatusCode(HttpStatus.OK);
+            return true;
+        });
     }
 
 }
