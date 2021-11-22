6dffe6e0c1def139c1f831ea3387a6da54e4842a
Harry-Bro <likeharrybro@gmail.com>
2021-11-22 10:44:14 +0900

    :ambulance: Fix cors

--- gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
+++ gateway-service/src/main/java/com/kati/gatewayservice/config/CorsConfig.java
@@ -1,22 +1,33 @@
 package com.kati.gatewayservice.config;
 
+import com.google.common.collect.ImmutableList;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
-public class CorsConfig implements WebFluxConfigurer {
+public class CorsConfig {
+//public class CorsConfig implements WebFluxConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-//                .allowCredentials(true)
-                .allowedOrigins(CorsConfiguration.ALL)
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .exposedHeaders(CorsConfiguration.ALL);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+////                .allowCredentials(true)
+//                .allowedOrigins(CorsConfiguration.ALL)
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .exposedHeaders(CorsConfiguration.ALL);
+//    }
 //
 //    @Order(0)
 //    @Bean
@@ -37,28 +48,27 @@ public class CorsConfig implements WebFluxConfigurer {
 //    private static final String ALLOWED_ORIGIN = "*";
 //    private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
 //
-//    @Bean
-//    public WebFilter corsFilter() {
-//        return (ServerWebExchange ctx, WebFilterChain chain) -> {
-//            ServerHttpRequest request = ctx.getRequest();
-//            if (CorsUtils.isCorsRequest(request)) {
-//                ServerHttpResponse response = ctx.getResponse();
-//                HttpHeaders headers = response.getHeaders();
-////                headers.add("Access-Control-Allow-Origin", "*");
-//                headers.setAccessControlAllowOrigin("*");
-////                headers.add("Access-Control-Allow-Methods", "*")
-//                headers.setAccessControlAllowMethods(ImmutableList.of(POST, DELETE, TRACE, HEAD, GET, OPTIONS, PATCH, PUT));
-////                headers.add("Access-Control-Allow-Headers", "*");
-//                headers.setAccessControlExposeHeaders(ImmutableList.of("*"));
-//                headers.add("Access-Control-Max-Age", MAX_AGE);
-//                headers.setAccessControlAllowCredentials(true);
-//                if (request.getMethod() == OPTIONS) {
-//                    response.setStatusCode(HttpStatus.OK);
-//                    return Mono.empty();
-//                }
-//            }
-//            return chain.filter(ctx);
-//        };
-//    }
+    @Order(1)
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+//                headers.add("Access-Control-Allow-Origin", "*");
+                headers.setAccessControlAllowOrigin("*");
+                headers.setAccessControlExposeHeaders(ImmutableList.of("*"));
+                headers.add("Access-Control-Allow-Methods", "*");
+//                headers.add("Access-Control-Allow-Headers", "*");
+                headers.setAccessControlAllowCredentials(true);
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
 
 }
