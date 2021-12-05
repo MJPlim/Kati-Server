fbdf21309d3431158c772b59f98785bdbc77286d
Harry-Bro <likeharrybro@gmail.com>
2021-12-05 18:44:19 +0900

    :fire: Remove unnecessary code

--- kati-core/src/main/java/com/kati/core/domain/user/api/RoleTestController.java
+++ kati-core/src/main/java/com/kati/core/domain/user/api/RoleTestController.java
@@ -3,7 +3,6 @@ package com.kati.core.domain.user.api;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleTestController {
 
     private final Environment env;
-    @Value("${message.hello}")
-    private final String testMessage;
 
     @ApiOperation(value = "유저 권한 테스트", notes = "유저의 권한을 테스트한다")
     @GetMapping("/api/v1/user")
@@ -31,7 +28,7 @@ public class RoleTestController {
 
     @GetMapping("/api/health/check")
     public String healthCheck() {
-        return testMessage;
+        return this.env.getProperty("message.hello");
     }
 
 }
